c9c297f28bdda51567bf8a58b07d62c293644102
Mizanur Rahman <mr.milonku@gmail.com>
2019-10-16 23:11:47 +0600

    Update Entities

--- DomainLayer/Entities/Calculations.cs
+++ DomainLayer/Entities/Calculations.cs
@@ -1,17 +1,26 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DomainLayer.Entities
 {
     public class Calculations : Entity
     {
-        public long UserId { get; set; }
-
+        [Required]
         public string FirstNumber { get; set; }
 
+        [Required]
         public string SecondNumber { get; set; }
 
+        [Required]
         public string Summation { get; set; }
 
+        [Required]
         public DateTime CalculatedOn { get; set; }
+
+        //Foreign key for Users
+        [ForeignKey("FK_Calculations_UserId")]
+        public long UserId { get; set; }
+        public Users User { get; set; }
     }
 }
--- DomainLayer/Entities/Users.cs
+++ DomainLayer/Entities/Users.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DomainLayer.Entities
 {
     public class Users : Entity
     {
+        [Required]
+        [MaxLength(50, ErrorMessage = "Cannot exceed 50 characters")]
         public string UserName { get; set; }
 
+        [Required]
         public DateTime CreatedOn { get; set; }
 
+        [Required]
         public bool IsActive { get; set; }
     }
 }
